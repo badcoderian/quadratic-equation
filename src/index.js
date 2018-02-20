a508bca617655ed8723a7420e03304a63e518423
ipriver <g1ran1q@gmail.com>
2018-02-20 14:07:43 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let [a, b, c] = splitter(equation)
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d))/(2*a)
+    x2 = (-b - Math.sqrt(d))/(2*a)
+  } else {
+    let rp = -b/(2*a);
+    let ip = Math.sqrt(-d)/(2*a);
+    x1 = rp + ip;
+    x2 = rp - ip; 
+  }
+  x1 = Math.round(x1);
+  x2 = Math.round(x2);
+  return [x1, x2].sort((a, b) => a - b);
+}
+
+function splitter(stri) {
+  stri = stri.split(' ');
+  let a = stri[0]
+  let i = stri.indexOf('x^2');
+  let b = stri[i + 1] + stri[i + 2];
+  let c = stri[stri.length - 2] + stri[stri.length - 1]
+  return [parseInt(a), parseInt(b), parseInt(c)];
 }
